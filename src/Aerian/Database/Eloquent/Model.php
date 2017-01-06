5acb959776adfdd4345e96eb61ff90cff9732745
Simon Neaves <simon@aerian.com>
2017-01-06 10:25:17 +0000

    list columns

--- src/Aerian/Database/Eloquent/Model.php
+++ src/Aerian/Database/Eloquent/Model.php
@@ -7,6 +7,18 @@ use Aerian\Blueprint\Adaptor\EloquentModel as BlueprintAdaptor;
 
 class Model extends EloquentModel
 {
+    /**
+     * the columns which should be included in a list view of this model, e.g. a REST index
+     * @var array
+     */
+    protected $_listColumns;
+
+    /**
+     * columns to exclude when the default list columns are set
+     * @var array
+     */
+    protected $_listColumnsBlacklist = ['id', 'created_at', 'updated_at'];
+
 
     public function blueprint()
     {
@@ -31,5 +43,51 @@ class Model extends EloquentModel
             ->getConnection()
             ->getDoctrineSchemaManager()
             ->listTableColumns($this->getTable());
+
+    /**
+     * @return array
+     */
+    public function getListColumns()
+    {
+        if (!isset($this->_listColumns)) {
+            $this->_setDefaultListColumns();
+        }
+        return $this->_listColumns;
     }
+
+    /**
+     * @param mixed $listColumns
+     * @return Model
+     */
+    public function setListColumns($listColumns)
+    {
+        $this->_listColumns = $listColumns;
+        return $this;
+    }
+
+    protected function _setDefaultListColumns()
+    {
+        $allColumns = array_keys($this->describe());
+        $columns = array_values(array_diff($allColumns, $this->getListColumnsBlacklist()));
+        $this->setListColumns($columns);
+    }
+
+    /**
+     * @return array
+     */
+    public function getListColumnsBlacklist()
+    {
+        return $this->_listColumnsBlacklist;
+    }
+
+    /**
+     * @param array $listColumnsBlacklist
+     * @return Model
+     */
+    public function setListColumnsBlacklist($listColumnsBlacklist)
+    {
+        $this->_listColumnsBlacklist = $listColumnsBlacklist;
+        return $this;
+    }
+
 }
